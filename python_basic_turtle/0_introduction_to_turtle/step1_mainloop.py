44af05b1fc085828889cfe52af0e8ca280af854c
Watanabe Naoki <asterisk37n@gmail.com>
2023-05-12 22:44:53 +0900

    change dir structure

---
+++ python_basic_turtle/0_introduction_to_turtle/step1_mainloop.py
@@ -0,0 +1,3 @@
+from turtle import *
+
+mainloop()  # これがないと、ファイルで実行したとき、すぐに終了してしまう
--- python_basic_turtle/0_introduction_to_turtle/step8_pendown_penup.py
+++ python_basic_turtle/0_introduction_to_turtle/step8_pendown_penup.py
@@ -1,10 +1,13 @@
 from turtle import *
 
+pendown()  # ペンをさげる
 shape('turtle')  # turtle, circle, arrow, classic
 forward(100)
+penup()  # ペンをあげる
 right(90)
 forward(100)
 left(90)
+pendown()  # ペンをさげる
 forward(200)
 backward(50)
 penup()  # ペンをあげる
